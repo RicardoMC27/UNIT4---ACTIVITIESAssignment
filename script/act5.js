32e3c8c701ca48f35c71f4d276fb766dda8ad774
RicardoMC27 <ricardomascarell27@gmail.com>
2022-10-19 13:00:30 +0200

    Todo acabado menos la 9

--- script/act5.js
+++ script/act5.js
@@ -1,7 +1,40 @@
-var nombre = prompt("Dime tu USUARIO");
-
-var contraseña = prompt("Dime tu contraseña");
-
-if 
-
-
+let letnom = "abcdefghijklmnopqrstuvwxyz0123456789";
+var comp_nom;
+do{
+    comp_nom = true;
+    let nombre = prompt("Dime tu nombre");
+    for(let i=0; i<nombre.length; i++){
+        if(letnom.includes(nombre[i]) == false){
+            alert("ERROR en el nombre")
+            comp_nom = false
+        }
+    }
+}while(comp_nom == false)
+let mayus_password = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let min_password = "abcdefghijklmnopqrstuvwxyz"
+let num_validation_password = "0123456789"
+let mayus
+let minus
+let num
+let caracter
+do {
+    let contrasenya = prompt("Dime tu contraseña")
+    mayus = false
+    minus = false
+    num = false
+    caracter = false
+    for(let j=0; j<contrasenya.length; j++){
+        if(mayus_password.includes(contrasenya[j]) == true){
+            mayus = true
+        }
+        else if(min_password.includes(contrasenya[j]) == true){
+            minus = true
+        }
+        else if(num_validation_password.includes(contrasenya[j]) == true){
+            num = true
+        }
+        else{
+            caracter = true
+        }
+    }
+}while(mayus==false || minus==false || num==false || caracter==false)
--- script/act9.js
+++ script/act9.js
@@ -1,16 +1,27 @@
 var nombre;
-var fin = "fin";
-while(nombre!=fin){
+var fin = "end";
+var arrayvidi = []
+var estuduiantes = []
+var grupo1 = new Array(4);
+var grupo2 = new Array(4);
+var grupo3 = new Array(4);
+var grupo4 = new Array(4);
+var grupo5 = new Array(4);
 
+while(nombre!=fin){
+    
     nombre = prompt("Dime un nombre de un Estudiante");
 
     if (nombre == fin){
         break;
     }
 
-    if (nombre==nombre){
+    
+    if (nombre=){
+        alert("Se ha repetdo los nombres");
         continue;
     }
-
     
+    estuduiantes[nombre];
+
 }
